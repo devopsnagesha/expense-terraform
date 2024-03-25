4855d3723936407970fecdfabb0f4333cd831aae
nagesha technology <asbconstruction19@gmail.com>
2024-03-25 16:04:17 +0530

    project setup -v1 ansible pull

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "instance" {
 
   tags ={
     Name = var.component
+    env = var.env
   }
 
 }
@@ -21,7 +22,7 @@ resource "null_resource" "ansible" {
 
     inline = [
       "sudo pip3.11 install ansible",
-      "ansible-playbook -i localhost; -U https://github.com/devopsnagesha/dev-expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component}"
+      "ansible-pull -i localhost, -U https://github.com/devopsnagesha/dev-expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component}"
     ]
   }
 }
