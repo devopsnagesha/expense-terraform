d8877a7ab1ebbf628e3f609ccfd007fe41a0c890
nagesha technology <asbconstruction19@gmail.com>
2024-03-29 20:07:11 +0530

    expense ansible new

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 module "frontend" {
+  depends_on    = [module.backend]
+
   source        = "./modules/app"
   instance_type = var.instance_type
   component     = "frontend"
@@ -10,6 +12,7 @@ module "frontend" {
 
 module "backend" {
   depends_on    = [module.mysql]
+
   source        = "./modules/app"
   instance_type = var.instance_type
   component     = "backend"
