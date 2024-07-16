65a412579f3f0c9795c007f5d46195b4b95e7d69
nagesha technology <asbconstruction19@gmail.com>
2024-07-16 18:43:40 +0530

    Adding ci steps, user dada and some other changes

--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-pip3.11 install ansible hvac &>>/opt/ansible.log
-ansible-pull -i localhost, -U https://github.com/nageshadevops/expense-ansible get-secrets.yml -e env=${env} -e role_name=${component}  -e vault_token=${vault_token} &>>/opt/ansible.log
-ansible-pull -i localhost, -U https://github.com/nageshadevops/expense-ansible expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json &>>/opt/ansible.log
-#
+ansible-pull -i localhost, -U https://github.com/devopsnagesha/expense-ansible get-secrets.yml -e env=${env} -e role_name=${component}  -e vault_token=${vault_token} &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/devopsnagesha/expense-ansible expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json &>>/opt/ansible.log
